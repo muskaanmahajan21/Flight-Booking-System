8a91558939a3fd29055a39e87a9db8643facad6a
muskaanmahajan21 <muskaanmahajan79@gmailcom>
2025-12-15 16:59:26 +0530

    correct index-login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Index";
+import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import { useAuth } from "./auth/AuthContext";
